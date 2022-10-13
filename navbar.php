ff7d1f2eb07f3e7ba6355ff24f3a1aabb0eecf2c
SumaSahithi-22 <114988284+SumaSahithi-22@users.noreply.github.com>
2022-10-13 02:30:31 -0500

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,6 +1,6 @@
 <!-- navbar --> 
       <nav class="navbar navbar-expand-md  bg-light" style="border:outset ;">
-      <a class="navbar-brand" href="index.php" style="color:#0038e0;">BRAINSTROM BANK</a>
+      <a class="navbar-brand" href="index.php" style="color:#0038e0;">MSS BANK</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
         <span class="navbar-toggler-icon"></span>
       </button>
